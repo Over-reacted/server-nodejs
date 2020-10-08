761dbb33bfdc358c842c573c1934b9c794dc119e
TodorTotev <51530311+TodorTotev@users.noreply.github.com>
2020-10-08 19:57:27 +0300

    setup db connection

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,15 +1,35 @@
+require("dotenv").config();
 import "reflect-metadata";
 import express from "express";
 import "express-async-errors";
 import cors from "cors";
 import compression from "compression";
+import mongoose from "mongoose";
+
 import { NotFoundError } from "../errors";
 import { Keys } from "../config";
 import { errorHandler, requestLoggerMiddleware } from "../middleware";
 import { AppRouter } from "./AppRouter";
-import "../modules/User/UserController";
+
+if (!Keys.dbHost) throw new Error("Connection string is required!");
 
 const app = express();
+mongoose
+  .connect(Keys.dbHost, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    /** ready to use. The `mongoose.connect()` promise resolves to undefined. */
+  })
+  .catch((err) => {
+    console.log(
+      `MongoDB connection error. Please make sure MongoDB is running. ${err}`
+    );
+    // process.exit();
+  });
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
