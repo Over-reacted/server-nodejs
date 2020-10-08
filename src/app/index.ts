fccc1fb726428249b61f97fad68149125cfb0db9
TodorTotev <51530311+TodorTotev@users.noreply.github.com>
2020-10-08 19:31:53 +0300

    cleanup

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,32 +1,25 @@
-import 'reflect-metadata';
-import express from 'express';
-import 'express-async-errors';
-import cookieSession from 'cookie-session';
-import cors from 'cors';
-import compression from 'compression';
-import { NotFoundError } from '../errors';
-import { Keys, corsOptions } from '../config';
-import { errorHandler, requestLoggerMiddleware } from '../middleware';
-import { AppRouter } from './AppRouter';
-import '../modules/User/UserController';
+import "reflect-metadata";
+import express from "express";
+import "express-async-errors";
+import cors from "cors";
+import compression from "compression";
+import { NotFoundError } from "../errors";
+import { Keys } from "../config";
+import { errorHandler, requestLoggerMiddleware } from "../middleware";
+import { AppRouter } from "./AppRouter";
+import "../modules/User/UserController";
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(compression());
 app.use(requestLoggerMiddleware);
-app.use(
-    cookieSession({
-        maxAge: 30 * 24 * 60 * 60 * 1000,
-        keys: [Keys.cookieKey],
-    })
-);
 
 app.use(AppRouter.getInstance());
 
-app.all('*', async (req, res, next) => {
-    throw new NotFoundError();
+app.all("*", async (req, res, next) => {
+  throw new NotFoundError();
 });
 app.use(errorHandler);
 
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
-import app from './app';
+import app from "./app";
+import { Keys } from "./config";
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}`);
+  console.log(`App listening on port ${PORT}`);
 });
