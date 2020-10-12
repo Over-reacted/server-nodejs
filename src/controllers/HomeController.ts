8f8434a0f0ede079e102c69cd511d6ed92796d6c
TodorTotev <51530311+TodorTotev@users.noreply.github.com>
2020-10-12 20:17:19 +0300

    not needed

--- src/controllers/HomeController.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from 'express';
-
-class HomeController {
-    getHome(req: Request, res: Response) {
-        res.send({ test: 'gosho' });
-    }
-}
-
-export { HomeController };
