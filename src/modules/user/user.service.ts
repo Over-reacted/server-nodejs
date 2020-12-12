ba3f3e2b7f30e629d4e3cce493f1a34ea750bb7a
TodorTotev <51530311+TodorTotev@users.noreply.github.com>
2020-12-12 11:51:44 +0200

    add sample user service

---
+++ src/modules/user/user.service.ts
@@ -0,0 +1,26 @@
+import { Injectable, NotAcceptableException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { User, UserFillableFields } from './user.entity';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+
+  async create(payload: UserFillableFields) {
+    const id = 1;
+    // Id is 1 for mocking purpose, we should decrypt the JWT somewhere along the chain
+    // and pass the id here
+    const user = await this.userRepository.findOne(id);
+
+    if (user) {
+      throw new NotAcceptableException('You only live once');
+    }
+
+    return await this.userRepository.save(this.userRepository.create(payload));
+  }
+}
