26aee76c5db18af8f047882cfa32736f918be1c3
TodorTotev <51530311+TodorTotev@users.noreply.github.com>
2020-12-12 11:48:42 +0200

    create user entity with base columns

---
+++ src/modules/user/user.entity.ts
@@ -0,0 +1,27 @@
+import { Exclude } from 'class-transformer';
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { PasswordTransformer } from './password.transfomer';
+
+@Entity({
+  name: 'users',
+})
+export class User {
+  @PrimaryGeneratedColumn()
+    id!: number;
+
+  @Column({ length: 255 })
+    email!: string;
+
+  @Column({
+        name: 'password',
+        length: 255,
+        transformer: new PasswordTransformer(),
+    })
+    @Exclude()
+    password!: string;
+}
+
+export class UserFillableFields {
+  email!: string;
+  password!: string;
+}
