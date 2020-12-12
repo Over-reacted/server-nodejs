e345b45c7b45c8bcaaa63bad18bc9726267655fc
TodorTotev <51530311+TodorTotev@users.noreply.github.com>
2020-12-12 11:11:11 +0200

    empty the whole repo

--- src/config/keys.ts
+++
@@ -1,9 +0,0 @@
-interface KeyProps {
-  jwtSecret: string;
-  dbHost: string | undefined;
-}
-
-export const Keys: KeyProps = {
-  jwtSecret: process.env.JWT_SECRET ?? "asdf",
-  dbHost: process.env.DB_HOST,
-};
