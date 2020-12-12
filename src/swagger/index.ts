8af16b1fb6ecbd0891299436b06d1eb71b2a8f2b
TodorTotev <51530311+TodorTotev@users.noreply.github.com>
2020-12-12 10:45:38 +0200

    add swagger

---
+++ src/swagger/index.ts
@@ -0,0 +1,18 @@
+import { INestApplication } from "@nestjs/common";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
+
+export const setupSwagger = (app: INestApplication) => {
+  const options = new DocumentBuilder()
+    .setTitle(SWAGGER_API_NAME)
+    .setDescription(SWAGGER_API_DESCRIPTION)
+    .setVersion(SWAGGER_API_CURRENT_VERSION)
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup(SWAGGER_API_ROOT, app, document);
+};
+
+export const SWAGGER_API_ROOT = "api/docs";
+export const SWAGGER_API_NAME = "Simple API";
+export const SWAGGER_API_DESCRIPTION = "Simple API Description";
+export const SWAGGER_API_CURRENT_VERSION = "1.0";
