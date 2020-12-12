8f4fc444c31fa67687e8e0d15c2cfc8b1af39f56
TodorTotev <51530311+TodorTotev@users.noreply.github.com>
2020-12-12 11:57:11 +0200

    refactor the create function since you wont be passing a jwt upon register

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -22,14 +22,21 @@ export class UsersService {
       .getOne();
   }
 
+  async getByEmail(email: string) {
+    return await this.userRepository
+      .createQueryBuilder('users')
+      .where('users.email = :email')
+      .setParameter('email', email)
+      .getOne();
+  }
+
   async create(payload: UserFillableFields) {
-    const id = 1;
-    // Id is 1 for mocking purpose, we should decrypt the JWT somewhere along the chain
-    // and pass the id here
-    const user = await this.userRepository.findOne(id);
+    const user = await this.getByEmail(payload.email);
 
     if (user) {
-      throw new NotAcceptableException('You only live once');
+      throw new NotAcceptableException(
+        'User with provided email already created.',
+      );
     }
 
     return await this.userRepository.save(this.userRepository.create(payload));
