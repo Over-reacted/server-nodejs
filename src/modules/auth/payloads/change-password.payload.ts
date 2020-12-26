fd963bcd90362894e6ad2cdb26b58218bc3a647e
Gyulshen Abaz <weirdoo2001@gmail.com>
2020-12-26 18:42:00 +0200

    Add payload for changing password for authenticated users

---
+++ src/modules/auth/payloads/change-password.payload.ts
@@ -0,0 +1,26 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsNotEmpty, MinLength } from "class-validator";
+import { Match } from "../decorators/match.decorator";
+
+export class ChangePasswordPayload {
+  @ApiProperty({
+    required: true,
+  })
+  @IsNotEmpty()
+  @MinLength(5)
+  currentPassword!: string;
+    
+  @ApiProperty({
+    required: true,
+  })
+  @IsNotEmpty()
+  @MinLength(5)
+  newPassword!: string;
+
+  @ApiProperty({
+    required: true,
+  })
+  @IsNotEmpty()
+  @Match('password', { message: "Confirm password doesn't match" })
+  confirmPassword!: string;
+}
